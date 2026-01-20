813bb3cdcf170123331d7dcec680d50aedb29c7d
ConnerGordon <2260684@flourbluffschools.org>
2026-01-20 11:57:27 -0600

    create base

--- enmy/enemy.gd
+++ enmy/enemy.gd
@@ -3,6 +3,8 @@ extends PathFollow3D
 ## speed of the enemy. what else?
 @export var speed:= 10.0
 
+
+@onready var base : Base = get_tree().get_first_node_in_group("base")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,3 +14,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	progress += delta* speed
+	
+	if progress_ratio > 1.0:
+		base.take_damage()
+		queue_free()
